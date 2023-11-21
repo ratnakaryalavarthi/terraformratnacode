538753d7e85ec4ee588ebb5b4f7f98cc75d77512
ratnakaryalavarthi <47362043+ratnakaryalavarthi@users.noreply.github.com>
2023-11-21 20:45:38 +0530

    Update main.tf with updated provider information

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
 provider "azurerm" {
   features {}
-}
+
 resource "azurerm_resource_group" "firsrg" {
   name     = var.resource_group_name
   location = var.location
